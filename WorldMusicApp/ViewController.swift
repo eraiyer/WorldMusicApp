a304e7645139e547236454ac87a23b1592a3ba85
eraiyer <eraiyer@gmail.com>
2016-07-11 15:18:03 -0700

    with mapKit and pin

--- WorldMusicApp/ViewController.swift
+++ WorldMusicApp/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
+        let initialLocation = CLLocation(latitude: 36.7783, longitude: -119.4179)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centerMapOnLocation(initialLocation)
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let regionRadius: CLLocationDistance = 7000000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 1.0, regionRadius * 1.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-
-
+    
+    func showAddViewController(placemark:CLPlacemark){
+        self.performSegueWithIdentifier("add", sender: placemark)
+    }
+    @IBAction func addPin(sender: UILongPressGestureRecognizer) {
+        let location = sender.locationInView(self.mapView)
+        
+        let locationCoord = self.mapView.convertPoint(location, toCoordinateFromView: self.mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoord
+        
+        self.mapView.removeAnnotations(mapView.annotations)
+        self.mapView.addAnnotation(annotation)
+    }
 }
 
